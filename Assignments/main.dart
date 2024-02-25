9332d143bf237a11f854317eb5e1ad5176dbad27
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-02-25 10:09:17 +0000

    Create main.dart

---
+++ Assignments/main.dart
@@ -0,0 +1,29 @@
+//Add Two Numbers
+int addTwo(int a, int b) {
+  return a + b;
+}
+
+//Subtract Two Numbers
+int subtractTwo(int a, int b) {
+  return a - b;
+}
+
+//Multiply Two Numbers
+int multiplyTwo(int a, int b) {
+  return a * b;
+}
+
+//Divide Two Numbers
+double divideTwo(int a, int b) {
+  return a / b;
+}
+
+//Get String Length
+int stringLength(String str) {
+  return str.length;
+}
+
+//Get First Element of a List
+T getFirstElement<T>(List<T> list) {
+  return list.first;
+}
