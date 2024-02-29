b55a75a22f5a64d705c8d16b95b91672adc2d289
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-02-29 05:35:07 +0000

    CONTROL FLOW

---
+++ control flow/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  int number = 1;
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else if (number == 10) {
+    print("Your number is equal to 10");
+  }
+}
